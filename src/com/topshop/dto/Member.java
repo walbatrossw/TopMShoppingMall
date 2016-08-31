4e4420e0c434b1c1518662913400a9342941cba4
202-09 <202-09@202-09-PC>
2016-08-31 11:16:28 +0900

    https://github.com/walbatrossw/TopMShoppingMall.git

---
+++ src/com/topshop/dto/Member.java
@@ -0,0 +1,62 @@
+package com.topshop.dto;
+
+public class Member {
+	private String mId = null;
+	private String mPw = null;
+	private String mName = null;
+	private int mLevel = 0;
+	private String mAddr = null;
+	private int mAge = 0;
+	private String mPhone = null;
+	private String mEmail = null;
+	public String getmId() {
+		return mId;
+	}
+	public void setmId(String mId) {
+		this.mId = mId;
+	}
+	public String getmPw() {
+		return mPw;
+	}
+	public void setmPw(String mPw) {
+		this.mPw = mPw;
+	}
+	public String getmName() {
+		return mName;
+	}
+	public void setmName(String mName) {
+		this.mName = mName;
+	}
+	public int getmLevel() {
+		return mLevel;
+	}
+	public void setmLevel(int mLevel) {
+		this.mLevel = mLevel;
+	}
+	public String getmAddr() {
+		return mAddr;
+	}
+	public void setmAddr(String mAddr) {
+		this.mAddr = mAddr;
+	}
+	public int getmAge() {
+		return mAge;
+	}
+	public void setmAge(int mAge) {
+		this.mAge = mAge;
+	}
+	public String getmPhone() {
+		return mPhone;
+	}
+	public void setmPhone(String mPhone) {
+		this.mPhone = mPhone;
+	}
+	public String getmEmail() {
+		return mEmail;
+	}
+	public void setmEmail(String mEmail) {
+		this.mEmail = mEmail;
+	}
+	
+	
+}
---
+++ src/com/topshop/dto/Product.java
@@ -0,0 +1,54 @@
+package com.topshop.dto;
+
+public class Product {
+	private String pCode = null;
+	private String pCate = null;
+	private String pName = null;
+	private int pCount = 0;
+	private String pDate = null;
+	private String pDetail = null;
+	private String mId = null;
+	public String getpCode() {
+		return pCode;
+	}
+	public void setpCode(String pCode) {
+		this.pCode = pCode;
+	}
+	public String getpCate() {
+		return pCate;
+	}
+	public void setpCate(String pCate) {
+		this.pCate = pCate;
+	}
+	public String getpName() {
+		return pName;
+	}
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+	public int getpCount() {
+		return pCount;
+	}
+	public void setpCount(int pCount) {
+		this.pCount = pCount;
+	}
+	public String getpDate() {
+		return pDate;
+	}
+	public void setpDate(String pDate) {
+		this.pDate = pDate;
+	}
+	public String getpDetail() {
+		return pDetail;
+	}
+	public void setpDetail(String pDetail) {
+		this.pDetail = pDetail;
+	}
+	public String getmId() {
+		return mId;
+	}
+	public void setmId(String mId) {
+		this.mId = mId;
+	}
+	
+}
